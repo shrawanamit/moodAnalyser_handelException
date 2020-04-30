022df4c017e9adad63e4b5aced8a740a34c613f0
shrawanamit <amitkumar06111@gmail.com>
2020-05-01 02:44:09 +0530

    MoodAnalyser. [refacter,repeat tc 1.1,1.2] created constructer to set message

--- src/main/java/com/ExceptionJunit/MoodAnalyser.java
+++ src/main/java/com/ExceptionJunit/MoodAnalyser.java
@@ -1,7 +1,12 @@
 package com.ExceptionJunit;
 
 public class MoodAnalyser {
+    private String message;
+    public MoodAnalyser(){}
 
+    public MoodAnalyser(String message){
+        this.message=message;
+    }
     public String analyseMood(String message) {
         if(message.contains("sad")) {
             return "SAD";
