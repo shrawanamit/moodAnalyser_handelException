d938bc6a8dd73f40842f86ef306b00cb2a9c3f32
shrawanamit <amitkumar06111@gmail.com>
2020-05-01 02:29:42 +0530

    MoodAnalyser. [UseCase 1.2] i am in any mood return happy

--- src/test/java/com/ExceptionJunit/MoodAnalyserTest.java
+++ src/test/java/com/ExceptionJunit/MoodAnalyserTest.java
@@ -11,4 +11,10 @@ public class MoodAnalyserTest {
         String mood_status=moodAnalyser.analyseMood("I am in sad mood");
         Assert.assertEquals("SAD", mood_status);
     }
+    @Test
+    public void should_return_HAPPY_if_message_not_contains_sad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood_status = moodAnalyser.analyseMood("I am in any mood");
+        Assert.assertEquals("HAPPY", mood_status);
+    }
 }
